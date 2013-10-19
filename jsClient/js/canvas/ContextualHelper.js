23a685a5e018291131019a0ff161928cc68181e1
Romina Suarez <rominas@vsisoft.com>
2013-10-19 16:00:42 -0200

    Objects can be erased using a contextual btn menu

--- jsClient/js/canvas/ContextualHelper.js
+++ jsClient/js/canvas/ContextualHelper.js
@@ -1,33 +1,33 @@
-/*
-Class: ContextualHelper
-
-Creates a context menu or toolbar for an object 
-
-Parameters:
-	options - contains all parameters for the class
-		options.object - the object for which we will render a contextual menu or toolbar
-		options.type - the type of helper to render. Possible options are :
-					   * objectBar
-
-*/
+/**
+ * @class  ContextualHelper Creates a context menu or toolbar for an object 
+ * @param {object} target object to put the menu
+ * @param {object} canvasObject where to draw this (canvas element)
+ * @param {object} options      contains all parameters for the class
+ */
 function ContextualHelper(target,canvasObject,options){
 	this.target=target;
 	this.canvasObject=canvasObject;	
 	this.type=options.type;
-	if (this[this.type]!="undefined"){
-		this[this.type]();
+	this.clickAction=function(){
+		alert("Clicked");
 	}
+	if (this[this.type]!="undefined"){
+		this.element=this[this.type]();
+	}
+	return this;
 }
-ContextualHelper.prototype.draw = function(){
-
-}
-
-/*
-Function: objectBar
-Renders a very concise toolbar with only 2 buttons right now. Delete and close.
-	-> Delete will erase the object <br/>
-	-> Cancel will only erase the objectBar
-*/
+/**
+ * deleteBar Renders a very concise toolbar with only 2 buttons right now. Delete and close.
+ * @return {object}  Square with the delete button
+ */
 ContextualHelper.prototype.deleteBar = function(){
-	this.canvasObject.drawSquare({height:50, width:50,y:this.target.y,x:this.target.x+this.target.width-50}).drawText({x:10,y:10,text:"Delete",fillStyle:"fill"});
+	var self=this;
+	this.clickAction= function(){
+		self.target.erase();
+	};
+	//includeInStructure is used to mark those object that are not supposed to be in the structure of the canvas object.This is particulary useful if you do not want the objects to reappear on redraw
+	return this.canvasObject.drawSquare({includeInStructure:false,height:50, width:50,y:this.target.y,x:this.target.x+this.target.width-50}).drawText({x:10,y:10,text:"Delete",fillStyle:"fill"});
+}
+ContextualHelper.prototype.executeClickAction = function (){
+	this.clickAction();
 }
